3299a231a3619f293aa21be04b4c169708aa02b2
元彦 <yuanyan.cao@alibaba-inc.com>
2015-04-17 02:38:57 +0800

    Add dist files

---
+++ dist/powers.js
@@ -0,0 +1,479 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Powers=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+var React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global.React : null);
+var transitionEvents = require('react-kit/transitionEvents');
+var appendVendorPrefix = require('react-kit/appendVendorPrefix');
+var animation = require('./animations/bounce');
+var showModalAnimation = animation.showModalAnimation;
+var hideModalAnimation = animation.hideModalAnimation;
+var showBackdropAnimation = animation.showBackdropAnimation;
+var hideBackdropAnimation = animation.hideBackdropAnimation;
+var showContentAnimation = animation.showContentAnimation;
+var hideContentAnimation = animation.hideContentAnimation;
+
+module.exports = React.createClass({displayName: "exports",
+    propTypes: {
+        className: React.PropTypes.string,
+        // Close the modal when esc is pressed? Defaults to true.
+        keyboard: React.PropTypes.bool,
+        onShow: React.PropTypes.func,
+        onHide: React.PropTypes.func,
+        backdrop: React.PropTypes.oneOfType([
+            React.PropTypes.bool,
+            React.PropTypes.string
+        ])
+    },
+
+    getDefaultProps: function() {
+        return {
+            className: "",
+            onShow: function(){},
+            onHide: function(){},
+            keyboard: true,
+            backdrop: true
+        };
+    },
+
+    getInitialState: function(){
+        return {
+            willHidden: false,
+            hidden: true
+        }
+    },
+
+    hasHidden: function(){
+        return this.state.hidden;
+    },
+
+    componentDidMount: function(){
+        var node = this.refs.modal.getDOMNode();
+        var endListener = function(e) {
+            if (e && e.target !== node) {
+                return;
+            }
+            transitionEvents.removeEndEventListener(node, endListener);
+            this.enter();
+
+        }.bind(this);
+        transitionEvents.addEndEventListener(node, endListener);
+    },
+
+    render: function() {
+
+        var hidden = this.hasHidden();
+        if(hidden) return null;
+
+        var willHidden = this.state.willHidden;
+
+        var modalStyle = appendVendorPrefix({
+            position: "fixed",
+            width: "500px",
+            transform: "translate3d(-50%, -50%, 0)",
+            top: "50%",
+            left: "50%",
+            backgroundColor: "white",
+            zIndex: 1050,
+            animationDuration: '0.4s',
+            animationFillMode: 'forwards',
+            animationName: willHidden? hideModalAnimation: showModalAnimation,
+            animationTimingFunction: 'cubic-bezier(0.7,0,0.3,1)'
+        });
+
+        var backdropStyle = appendVendorPrefix({
+            position: "fixed",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 1040,
+            backgroundColor: "black",
+            animationDuration: '0.4s',
+            animationFillMode: 'forwards',
+            animationName: willHidden? hideBackdropAnimation: showBackdropAnimation,
+            animationTimingFunction: 'cubic-bezier(0.7,0,0.3,1)'
+        });
+
+        var contentStyle = appendVendorPrefix({
+            animationDuration: '0.4s',
+	        animationFillMode: 'forwards',
+            animationDelay: '0.25s',
+            animationName: showContentAnimation,
+	        animationTimingFunction: 'cubic-bezier(0.7,0,0.3,1)'
+        });
+
+        var backdrop = this.props.backdrop? React.createElement("div", {onClick: this.hide, style: backdropStyle}): undefined;
+
+
+        if(willHidden) {
+            var node = this.refs.modal.getDOMNode();
+            var endListener = function(e) {
+                if (e && e.target !== node) {
+                    return;
+                }
+
+                transitionEvents.removeEndEventListener(node, endListener);
+                this.leave();
+
+            }.bind(this);
+            transitionEvents.addEndEventListener(node, endListener);
+        }
+
+        return (React.createElement("span", null, 
+            React.createElement("div", {ref: "modal", style: modalStyle, tabIndex: "-1", className: this.props.className}, 
+                React.createElement("div", {style: contentStyle}, 
+                    this.props.children
+                )
+            ), 
+            backdrop
+         ))
+        ;
+    },
+
+    leave: function(){
+        this.setState({
+            hidden: true
+        });
+        this.props.onHide();
+    },
+
+    enter: function(){
+        this.props.onShow();
+    },
+
+    show: function(){
+        if(!this.hasHidden()) return;
+
+        this.setState({
+            willHidden: false,
+            hidden: false
+        });
+    },
+
+    hide: function(){
+
+        if(this.hasHidden()) return;
+
+        this.setState({
+            willHidden: true
+        });
+    },
+
+    toggle: function(){
+        if(this.hasHidden())
+            this.show();
+        else
+            this.hide();
+    },
+
+    listenKeyboard: function(event) {
+        if (this.props.keyboard &&
+                (event.key === "Escape" ||
+                 event.keyCode === 27)) {
+            this.hide();
+        }
+    },
+
+    componentDidMount: function() {
+        window.addEventListener("keydown", this.listenKeyboard, true);
+    },
+
+    componentWillUnmount: function() {
+        window.removeEventListener("keydown", this.listenKeyboard, true);
+    },
+
+});
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./animations/bounce":8,"react-kit/appendVendorPrefix":2,"react-kit/transitionEvents":7}],2:[function(require,module,exports){
+'use strict';
+
+var getVendorPropertyName = require('./getVendorPropertyName');
+
+module.exports = function (target, sources){
+    var to = Object(target);
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+    for (var nextIndex = 1; nextIndex < arguments.length; nextIndex++) {
+        var nextSource = arguments[nextIndex];
+        if (nextSource == null) {
+            continue;
+        }
+
+        var from = Object(nextSource);
+
+        for (var key in from) {
+            if (hasOwnProperty.call(from, key)) {
+                to[key] = from[key];
+            }
+        }
+    }
+
+    var prefixed = {};
+    for (var key in to) {
+        prefixed[getVendorPropertyName(key)] = to[key]
+    }
+
+    return prefixed
+}
+
+},{"./getVendorPropertyName":4}],3:[function(require,module,exports){
+'use strict';
+
+var cssVendorPrefix;
+
+module.exports = function (){
+
+    if(cssVendorPrefix) return cssVendorPrefix;
+
+    var styles = window.getComputedStyle(document.documentElement, '');
+    var pre = (Array.prototype.slice.call(styles).join('').match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+    )[1];
+
+    return cssVendorPrefix = '-' + pre + '-';
+}
+
+},{}],4:[function(require,module,exports){
+'use strict';
+
+var div = document.createElement('div');
+var prefixes = ['Moz', 'Webkit', 'O', 'ms'];
+var domVendorPrefix;
+
+// Helper function to get the proper vendor property name. (transition => WebkitTransition)
+module.exports = function (prop) {
+
+    if (prop in div.style) return prop;
+
+    var prop = prop.charAt(0).toUpperCase() + prop.substr(1);
+    if(domVendorPrefix){
+        return domVendorPrefix + prop;
+    }else{
+        for (var i=0; i<prefixes.length; ++i) {
+            var vendorProp = prefixes[i] + prop;
+            if (vendorProp in div.style) {
+                domVendorPrefix = prefixes[i];
+                return vendorProp;
+            }
+        }
+    }
+}
+
+},{}],5:[function(require,module,exports){
+'use strict';
+
+var insertRule = require('./insertRule');
+var vendorPrefix = require('./getVendorPrefix')();
+var index = 0;
+
+module.exports = function (keyframes) {
+    // random name
+    var name = 'anim_'+ (++index) + (+new Date);
+    var css = "@" + vendorPrefix + "keyframes " + name + " {";
+
+    for (var key in keyframes) {
+        css += key + " {";
+
+        for (var property in keyframes[key]) {
+            var part = ":" + keyframes[key][property] + ";";
+            // We do vendor prefix for every property
+            css += vendorPrefix + property + part;
+            css += property + part;
+        }
+
+        css += "}";
+    }
+
+    css += "}";
+
+    insertRule(css);
+
+    return name
+}
+
+},{"./getVendorPrefix":3,"./insertRule":6}],6:[function(require,module,exports){
+'use strict';
+
+var extraSheet;
+
+module.exports = function (css) {
+
+    if (!extraSheet) {
+        // First time, create an extra stylesheet for adding rules
+        extraSheet = document.createElement('style');
+        document.getElementsByTagName('head')[0].appendChild(extraSheet);
+        // Keep reference to actual StyleSheet object (`styleSheet` for IE < 9)
+        extraSheet = extraSheet.sheet || extraSheet.styleSheet;
+    }
+
+    var index = (extraSheet.cssRules || extraSheet.rules).length;
+    extraSheet.insertRule(css, index);
+
+    return extraSheet;
+}
+
+},{}],7:[function(require,module,exports){
+'use strict';
+
+/**
+ * EVENT_NAME_MAP is used to determine which event fired when a
+ * transition/animation ends, based on the style property used to
+ * define that event.
+ */
+var EVENT_NAME_MAP = {
+  transitionend: {
+    'transition': 'transitionend',
+    'WebkitTransition': 'webkitTransitionEnd',
+    'MozTransition': 'mozTransitionEnd',
+    'OTransition': 'oTransitionEnd',
+    'msTransition': 'MSTransitionEnd'
+  },
+
+  animationend: {
+    'animation': 'animationend',
+    'WebkitAnimation': 'webkitAnimationEnd',
+    'MozAnimation': 'mozAnimationEnd',
+    'OAnimation': 'oAnimationEnd',
+    'msAnimation': 'MSAnimationEnd'
+  }
+};
+
+var endEvents = [];
+
+function detectEvents() {
+  var testEl = document.createElement('div');
+  var style = testEl.style;
+
+  // On some platforms, in particular some releases of Android 4.x,
+  // the un-prefixed "animation" and "transition" properties are defined on the
+  // style object but the events that fire will still be prefixed, so we need
+  // to check if the un-prefixed events are useable, and if not remove them
+  // from the map
+  if (!('AnimationEvent' in window)) {
+    delete EVENT_NAME_MAP.animationend.animation;
+  }
+
+  if (!('TransitionEvent' in window)) {
+    delete EVENT_NAME_MAP.transitionend.transition;
+  }
+
+  for (var baseEventName in EVENT_NAME_MAP) {
+    var baseEvents = EVENT_NAME_MAP[baseEventName];
+    for (var styleName in baseEvents) {
+      if (styleName in style) {
+        endEvents.push(baseEvents[styleName]);
+        break;
+      }
+    }
+  }
+}
+
+if(typeof window !== 'undefined'){
+  detectEvents();
+}
+
+
+// We use the raw {add|remove}EventListener() call because EventListener
+// does not know how to remove event listeners and we really should
+// clean up. Also, these events are not triggered in older browsers
+// so we should be A-OK here.
+
+function addEventListener(node, eventName, eventListener) {
+  node.addEventListener(eventName, eventListener, false);
+}
+
+function removeEventListener(node, eventName, eventListener) {
+  node.removeEventListener(eventName, eventListener, false);
+}
+
+module.exports = {
+  addEndEventListener: function(node, eventListener) {
+    if (endEvents.length === 0) {
+      // If CSS transitions are not supported, trigger an "end animation"
+      // event immediately.
+      window.setTimeout(eventListener, 0);
+      return;
+    }
+    endEvents.forEach(function(endEvent) {
+      addEventListener(node, endEvent, eventListener);
+    });
+  },
+
+  removeEndEventListener: function(node, eventListener) {
+    if (endEvents.length === 0) {
+      return;
+    }
+    endEvents.forEach(function(endEvent) {
+      removeEventListener(node, endEvent, eventListener);
+    });
+  }
+};
+
+},{}],8:[function(require,module,exports){
+var insertKeyframesRule = require('react-kit/insertKeyframesRule');
+
+module.exports = {
+    showModalAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 0,
+            transform: 'translate3d(-50%, -400px, 0)'
+        },
+        '100%': {
+            opacity: 1,
+            transform: 'translate3d(-50%, -50%, 0)'
+        }
+    }),
+
+    hideModalAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 1,
+            transform: 'translate3d(-50%, -50%, 0)'
+        },
+        '100%': {
+            opacity: 0,
+            transform: 'translate3d(-50%, 100px, 0)'
+        }
+    }),
+
+    showBackdropAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 0
+        },
+        '100%': {
+            opacity: 0.7
+        }
+    }),
+
+    hideBackdropAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 0.7
+        },
+        '100%': {
+            opacity: 0
+        }
+    }),
+
+    showContentAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 0,
+            transform: 'translate3d(0, -100px, 0)'
+        },
+        '100%': {
+            opacity: 1,
+            transform: 'translate3d(0, 0, 0)'
+        }
+    }),
+
+    hideContentAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 1,
+            transform: 'translate3d(0, 0, 0)'
+        },
+        '100%': {
+            opacity: 0,
+            transform: 'translate3d(0, 50px, 0)'
+        }
+    })
+}
+
+},{"react-kit/insertKeyframesRule":5}]},{},[1])(1)
+});
